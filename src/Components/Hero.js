de0893bb38da8da0fd182641cc5338bef8efed22
AJ <alexanderjessediaz@gmail.com>
2020-10-07 14:43:46 -0600

    add section, h1, and p tag to Hero.js, add Styling folder and add Hero.css in folder. In Hero.css add styling for h1 and p tag in Hero.

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,9 +1,12 @@
 import React from "react";
+import '../Styling/Hero.css'
 
 const Hero = () => (
-    <div>
-        <p>Hello World</p>
-    </div> 
+    <section>
+        <h1 id="main-header">How Was Your Day?</h1>
+        <p id="main-p">A place where you can jot down how you day was. Good, bad, meh, however your day was sum it up the way you want!</p>
+
+    </section>
 )
 
 export default Hero;
