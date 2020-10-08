3d501396232adb185e713ef2c31d8a371b31b62a
AJ <alexanderjessediaz@gmail.com>
2020-10-08 14:08:56 -0600

    add DevComments to src, remove Loading component, change App from function to class and add state with posts

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from "react-router-dom";
-import {Container} from "react-bootstrap";
-import { NavBar, Loading, PrivateRoute } from "./Components";
+import { Container } from "react-bootstrap";
+import { NavBar, PrivateRoute } from "./Components";
 import { Home, Profile, ExternalApi } from "./Views/Index";
-import { useAuth0 } from "@auth0/auth0-react";
 
-function App() {
-  const { isLoading } = useAuth0();
 
-  if (isLoading) {
-    return <Loading />;
+class App extends Component {
+  
+  state = {
+    posts:[]
   }
-  return (
-    <div className="App">
-      <NavBar />
-      <Container className="flex-grow-1 mt-5">
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <PrivateRoute path="/Profile" component={Profile} />
-          <PrivateRoute path="/external-api" component={ExternalApi} />
-        </Switch>
-      </Container>
-    </div>
-  );
+
+  render(){
+    
+    return (
+      <div className="App">
+        <NavBar />
+        <Container className="flex-grow-1 mt-5">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <PrivateRoute path="/Profile" component={Profile} />
+            <PrivateRoute path="/external-api" component={ExternalApi} />
+          </Switch>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Components/index.js
+++ src/Components/index.js
@@ -1,5 +1,4 @@
-import Loading from "./Loading";
 import NavBar from "./NavBar";
 import PrivateRoute from "./PrivateRoute";
 
-export { Loading, NavBar, PrivateRoute };
+export { NavBar, PrivateRoute };
