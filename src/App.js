0164c07c652873247828ee76d51b528fcea8b1a7
AJ <alexanderjessediaz@gmail.com>
2020-10-15 17:18:36 -0600

    make App a class, take out Loading component, comment in DevComments about this issue

--- src/App.js
+++ src/App.js
@@ -1,34 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Route, Switch } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import NavBar from "./Containers/NavBar";
-import { PrivateRoute, Loading } from "./AuthComponents"
-import { Home, Profile } from "./Views/index";
-import { useAuth0 } from "@auth0/auth0-react";
+import { PrivateRoute } from "./AuthComponents"
+import { Home, Profile, ExternalApi } from "./Views/index";
+
 import "./Styling/App.css"
+const postUrl = "http://localhost:4000/posts";
 
+class App extends Component {
 
-const App = () => {
+  state = {
+    posts:[]
+  };
+
+  componentDidMount(){
+    this.getPosts()
+  };
+
+  getPosts = () => {
+    fetch(postUrl)
+      .then(response => response.json())
+      .then(posts => this.setState({posts}))
+  };
+
+  addPost = (newPost) => {
+    this.setState({
+      posts: [...this.state.posts, newPost]
+    })
+  } 
   
-  const { isLoading } = useAuth0();
-
-  if (isLoading) {
-    return <Loading />;
-  }
-    
-    return (
-      <div className="App">
-        <NavBar />
-        <Container className="flex-grow-1 mt-5">
-          <Switch>
-            <Route path="/" exact component={Home} />
-            <PrivateRoute path="/Profile" component={Profile} />
-            {/* <PrivateRoute path="/external-api" component={ExternalApi} /> */}
-          </Switch>
-        </Container>
-      </div>
-    );
-  // }
+  
+    render(){
+      return (
+        <div className="App">
+          <NavBar />
+          <Container className="flex-grow-1 mt-5">
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <PrivateRoute path="/Profile" component={Profile} />
+              <PrivateRoute path="/external-api" component={ExternalApi} />
+            </Switch>
+          </Container>
+        </div>
+      );
+   }
 }
-
 export default App;
--- src/views/home.js
+++ src/views/home.js
@@ -3,29 +3,11 @@ import PostForm from '../Components/PostForm';
 import Hero from '../Containers/Hero';
 
 
-const postUrl = "http://localhost:4000/posts";
+
 
 class Home extends Component {
 
-  state = {
-    posts:[]
-  };
-
-  componentDidMount(){
-    this.getPosts()
-  };
-
-  getPosts = () => {
-    fetch(postUrl)
-      .then(response => response.json())
-      .then(posts => this.setState({posts}))
-  };
-
-  addPost = (newPost) => {
-    this.setState({
-      posts: [...this.state.posts, newPost]
-    })
-  } 
+  
 
   render(){
     return (
