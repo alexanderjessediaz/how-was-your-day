c987104c639871a15ff7fd1aa45838dbd51a58f1
AJ <alexanderjessediaz@gmail.com>
2020-10-27 10:30:40 -0600

    add edit button to userPosts set bool to false on click

--- src/Components/Post/UserPost.js
+++ src/Components/Post/UserPost.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Card, Button} from 'react-bootstrap';
 
 
 function UserPost({id, title,content, deletePost}) {
 
+  const [isToggled, setIsToggled] = useState(false)
+
   const handleClick = (e) => deletePost(id)
+
+  const handleToggle = (e) => setIsToggled(!isToggled)
   return (
     <Card className="user-post-card" style={{ width: '18rem' }}>
       <Card.Body >
@@ -16,6 +20,13 @@ function UserPost({id, title,content, deletePost}) {
           variant="info"
           onClick={handleClick}
           >Delete
+        </Button>
+        <Button 
+          size="sm"
+          name="edit-btn" 
+          variant="success"
+          onClick={handleToggle}
+          >Edit
         </Button>
       </Card.Body>
 
